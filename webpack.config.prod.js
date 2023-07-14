91901e5b4ee6cd9f7a8e69dec9f18c3b6d8c6dbc
Paulo Victor <paulovmel@gmail.com>
2023-07-14 00:20:54 -0300

    feat: creates webpack for production

---
+++ webpack.config.prod.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+const deployURL = process.env.DEPLOY_URL || "http://localhost:3003";
+
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: deployURL,
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "cart",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./Cart": "./src/components/Cart",
+        "./useCart": "./src/store/cart",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+        jotai: {
+          requiredVersion: deps["jotai"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
