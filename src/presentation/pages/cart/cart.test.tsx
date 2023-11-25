e3fb4c4d04e444ce314b46c0909c9fa6e15e5503
Paulo Victor <paulovmel@gmail.com>
2023-11-25 01:14:24 -0300

    test: adds new test cases to cart page

--- src/presentation/pages/cart/cart.test.tsx
+++ src/presentation/pages/cart/cart.test.tsx
@@ -1,40 +1,124 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { Cart } from ".";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
+import { createRandomProducts } from "../../../__tests__/__mocks__/domain/entities/mock-product";
+import productsController from "../../controllers/products-controller";
+import cartController from "../../controllers/cart-controller";
+
+// Mocking dependencies
+jest.mock("../../store/cart", () => ({
+  __esModule: true,
+  default: jest.fn(() => [[], jest.fn()]),
+}));
+
+jest.mock("../../../utils/formatCurrency", () => ({
+  __esModule: true,
+  formatCurrency: jest.fn(),
+}));
+
+jest.mock("../../controllers/cart-controller", () => ({
+  __esModule: true,
+  default: {
+    updateProductInCartQuantity: jest.fn(),
+    addProductToCart: jest.fn(),
+    removeProductFromCart: jest.fn(),
+  },
+}));
+
+jest.mock("../../controllers/products-controller", () => ({
+  __esModule: true,
+  default: {
+    getProductsSuggestions: jest.fn(() => Promise.resolve([])),
+  },
+}));
 
 describe("Cart", () => {
-  it("should render the page correctly", () => {
-    render(
-      <MemoryRouter>
-        <Cart />
-      </MemoryRouter>
-    );
-
-    expect(screen.getByText("Seu carrinho")).toBeInTheDocument();
-    expect(
-      screen.getByText("Nenhum produto adicionado ao carrinho!")
-    ).toBeInTheDocument();
-    expect(screen.getByText("Talvez você goste")).toBeInTheDocument();
-    expect(screen.getByText("Total com taxa: R$ 5,99")).toBeInTheDocument();
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  it("should be able to add a suggested product to cart", async () => {
+  it("should render Cart page with empty cart", async () => {
     render(
       <MemoryRouter>
         <Cart />
       </MemoryRouter>
     );
 
-    const suggestedProductsAddToCartButtons = screen.getAllByRole("button", {
-      name: "Adicionar ao carrinho",
+    expect(
+      screen.getByText(/Nenhum produto adicionado ao carrinho!/i)
+    ).toBeInTheDocument();
+  });
+
+  it("should render the Cart page with non-empty cart", async () => {
+    const productsMock = createRandomProducts(5);
+    jest
+      .spyOn(productsController, "getProductsSuggestions")
+      .mockResolvedValue([]);
+
+    act(() => {
+      jest
+        .requireMock("../../store/cart")
+        .default.mockReturnValue([productsMock, jest.fn()]);
     });
 
-    await userEvent.click(suggestedProductsAddToCartButtons[0]);
+    render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>
+    );
 
-    expect(
-      screen.queryByText("Total produtos: R$ 5,99")
-    ).not.toBeInTheDocument();
+    productsMock.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    });
+  });
+
+  it("should handle quantity increment and decrement", async () => {
+    const productsMock = createRandomProducts(1);
+    jest
+      .spyOn(productsController, "getProductsSuggestions")
+      .mockResolvedValue([]);
+
+    act(() => {
+      jest
+        .requireMock("../../store/cart")
+        .default.mockReturnValue([productsMock, jest.fn()]);
+    });
+
+    render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>
+    );
+
+    const incrementButton = screen.getByTestId(
+      `increment-button-${productsMock[0].productId}`
+    );
+
+    await userEvent.click(incrementButton);
+
+    expect(cartController.updateProductInCartQuantity).toHaveBeenCalledWith(
+      {
+        product: productsMock[0],
+        cart: productsMock,
+        setCart: expect.any(Function),
+      },
+      2
+    );
+
+    const decrementButton = screen.getByTestId(
+      `decrement-button-${productsMock[0].productId}`
+    );
+    await userEvent.click(decrementButton);
+
+    expect(cartController.updateProductInCartQuantity).toHaveBeenCalledWith(
+      {
+        product: productsMock[0],
+        cart: productsMock,
+        setCart: expect.any(Function),
+      },
+      0
+    );
   });
 });
