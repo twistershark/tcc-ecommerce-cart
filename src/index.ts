c70f468fb839af1539859b1a3458a4758ebc5164
Paulo Victor <paulovmel@gmail.com>
2023-11-24 21:42:33 -0300

    feat: moves react files to clean arch

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import("./App");
+import("./presentation/App");
--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import Cart from "./components/Cart";
 import "./index.css";
+import { Cart } from "./pages/cart";
 
 const App = () => (
   <BrowserRouter>
