44ab0b77f9e90d03fb29f1108e78eb255deacc74
Paulo Victor <paulovmel@gmail.com>
2023-11-25 02:57:39 -0300

    test: creates mock product service

---
+++ src/__tests__/__mocks__/domain/services/mock-products-service.ts
@@ -0,0 +1,12 @@
+import { Category, Product } from "../../../../domain/entities/product";
+import { ProductsServiceInterface } from "../../../../domain/services/products-service-interface";
+import { MockProductRepository } from "../../infrastructure/repositories/mock-products-repository";
+
+export class MockProductsService implements ProductsServiceInterface {
+  productRepository = new MockProductRepository();
+
+  async getProductsSuggestions(): Promise<Product[]> {
+    const products = await this.productRepository.getProductsSuggestions();
+    return products.slice(0, 4);
+  }
+}
